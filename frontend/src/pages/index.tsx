9e485e32bbcf055b08f9be0af55db6fb339312da
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-06-08 14:19:54 +0200

    Implemented basic frontend layout to display the fetched data from Sanity CMS

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,9 +1,44 @@
-const Index = () => {
+import Link from 'next/link'
+import groq from 'groq'
+import { client } from '../../client'
+import Head from 'next/head'
+
+const Index = ({ posts }) => {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <>
+      <Head>
+        <title>{posts.length} Posts</title>
+      </Head>
+      <h1 className='text-3xl my-4 font-bold flex justify-center items-center '>
+        Welcome to Innotopia's Blog!
+      </h1>
+      <div className='container mx-auto grid grid-cols-1 divide-y divide-blue-100'>
+        {posts.length > 0 &&
+          posts.map(
+            ({ _id, title = '', slug = '', publishedAt = '' }) =>
+              slug && (
+                <li key={_id} className='p-4 hover:bg-blue-50'>
+                  <Link href={`/post/${encodeURIComponent(slug.current)}`}>
+                    {title} ({new Date(publishedAt).toDateString()})
+                  </Link>{' '}
+                </li>
+              )
+          )}
+      </div>
+    </>
   )
+}
+
+export async function getStaticProps() {
+  const posts = await client.fetch(
+    groq`*[_type == "post" && publishedAt < now()] | order(publishedAt desc)`
+  )
+
+  return {
+    props: {
+      posts,
+    },
+  }
 }
 
 export default Index
--- frontend/src/pages/post/[slug].tsx
+++ frontend/src/pages/post/[slug].tsx
@@ -1,9 +1,32 @@
 import groq from 'groq'
 import imageUrlBuilder from '@sanity/image-url'
+import { PortableText } from '@portabletext/react'
 import { client } from '../../../client'
 
 function urlFor(source) {
   return imageUrlBuilder(client).image(source)
+}
+
+const ptComponents = {
+  types: {
+    image: ({ value }) => {
+      if (!value?.asset?._ref) {
+        return null
+      }
+      return (
+        <img
+          alt={value.alt || ' '}
+          loading='lazy'
+          src={urlFor(value)
+            .width(600)
+            .height(240)
+            .fit('max')
+            .auto('format')
+            .url()}
+        />
+      )
+    },
+  },
 }
 
 const Post = (props) => {
@@ -13,24 +36,27 @@ const Post = (props) => {
     categories,
     authorImage,
     mainImage,
+    body = [],
   } = props.post
 
   return (
     <article>
-      <h1 className='text-2xl font-semibold'>{title}</h1>
+      <h1 className='text-3xl font-semibold my-2'>{title}</h1>
 
       {mainImage && (
-        <div>
+        <div className='my-4'>
           <img src={urlFor(mainImage).width(300).url()} alt={title} />
         </div>
       )}
 
-      <p className='text-xl'>Posted in</p>
+      <p className='text-2xl'>Posted in</p>
 
       {categories && (
         <ul className='ml-2'>
           {categories.map((category) => (
-            <li key={category}>{category}</li>
+            <li key={category} className=''>
+              {category}
+            </li>
           ))}
         </ul>
       )}
@@ -42,6 +68,8 @@ const Post = (props) => {
           <img src={urlFor(authorImage).width(50).url()} alt={name} />
         </div>
       )}
+
+      <PortableText value={body} components={ptComponents} />
     </article>
   )
 }
@@ -51,7 +79,8 @@ const query = groq`*[_type == "post" && slug.current == $slug][0]{
   "name": author->name, 
   "categories": categories[] -> title, 
   "authorImage": author->image,
-  mainImage
+  mainImage,
+  body
   }`
 
 /* IMPORTANT
