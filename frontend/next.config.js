dc99f50deeb260158d4166a48f2429ad31e6f6e7
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-06-18 21:25:27 +0200

    Added a Custom Webpack Loader

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // Other configuration options
+  // This handles .ts and .tsx files outside of the Next.js directory! 👇🏻
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.tsx?$/,
+      use: {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+      exclude: /node_modules/,
+    })
+
+    return config
+  },
 }
 
 module.exports = nextConfig
