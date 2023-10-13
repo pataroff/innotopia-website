9247354453d90b2224ed67e83cd412bc4d7265bc
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-10-13 13:08:36 +0300

    Added 'favicon'

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import { Poppins } from 'next/font/google'
 import Nav from './common/Nav'
 import Footer from './common/Footer'
@@ -12,6 +14,13 @@ const poppins = Poppins({
 const Layout = ({ children }) => {
   return (
     <div className={`${poppins.variable}`}>
+      <Head>
+        <title>
+          Innotopia | Empowering Digital Transformation and Innovation
+          Acceleration
+        </title>
+        <link rel='icon' href='/favicon.png' />
+      </Head>
       <Nav />
       <main>{children}</main>
       <Footer />
