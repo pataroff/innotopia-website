552f8126743f0699d932f3538423c75ca5f30077
iwwan-01 <89984902+iwwan-01@users.noreply.github.com>
2023-10-18 22:59:53 +0300

    Minor deployment fix

--- frontend/src/pages/shopstory-block/[entryId].jsx
+++ frontend/src/pages/shopstory-block/[entryId].jsx
@@ -1,16 +1,10 @@
-import { Metadata, RenderableContent, ShopstoryClient } from '@shopstory/core'
-import { Shopstory, ShopstoryMetadataProvider } from '@shopstory/react'
-import type { GetStaticPaths, GetStaticProps, NextPage } from 'next'
-import { sanityClient } from '../../lib/sanity.client'
-import { shopstoryConfig } from '../../shopstory/config'
-import { DemoShopstoryProvider } from '../../shopstory/provider'
+import { Metadata, RenderableContent, ShopstoryClient } from '@shopstory/core';
+import { Shopstory, ShopstoryMetadataProvider } from '@shopstory/react';
+import { sanityClient } from '../../lib/sanity.client';
+import { shopstoryConfig } from '../../shopstory/config';
+import { DemoShopstoryProvider } from '../../shopstory/provider';
 
-type ShopstoryBlockPageProps = {
-  renderableContent: RenderableContent
-  meta: Metadata
-}
-
-const ShopstoryBlockPage: NextPage<ShopstoryBlockPageProps> = (props) => {
+const ShopstoryBlockPage = (props) => {
   return (
     //@ts-ignore
     <DemoShopstoryProvider>
@@ -18,64 +12,57 @@ const ShopstoryBlockPage: NextPage<ShopstoryBlockPageProps> = (props) => {
         <Shopstory content={props.renderableContent} />
       </ShopstoryMetadataProvider>
     </DemoShopstoryProvider>
-  )
-}
+  );
+};
 
-export const getStaticPaths: GetStaticPaths = () => {
-  return { paths: [], fallback: 'blocking' }
-}
+export const getStaticPaths = () => {
+  return { paths: [], fallback: 'blocking' };
+};
 
-export const getStaticProps: GetStaticProps<
-  ShopstoryBlockPageProps,
-  { entryId: string }
-> = async (context) => {
-  let { params, preview, locale = 'en' } = context
+export const getStaticProps = async (context) => {
+  let { params, preview, locale = 'en' } = context;
 
   if (!params) {
-    return { notFound: true }
+    return { notFound: true };
   }
 
   const rawContent = await fetchShopstoryContentJSONFromCMS(
     params.entryId,
     locale,
     !!preview
-  )
+  );
 
-  console.log(rawContent)
+  console.log(rawContent);
   // Returns an object with content.en properties directly inside of it!
 
   const shopstoryClient = new ShopstoryClient(shopstoryConfig, {
     locale,
     sanity: { preview },
-  })
-  const renderableContent = shopstoryClient.add(rawContent)
-  const meta = await shopstoryClient.build()
+  });
+  const renderableContent = shopstoryClient.add(rawContent);
+  const meta = await shopstoryClient.build();
 
   return {
     props: { renderableContent, meta },
     revalidate: 10,
-  }
-}
+  };
+};
 
-async function fetchShopstoryContentJSONFromCMS(
-  entryId: string,
-  locale: string,
-  preview: boolean
-): Promise<any> {
+async function fetchShopstoryContentJSONFromCMS() {
   const entryIdQuery =
-    preview && !entryId.startsWith('drafts.') ? `drafts.${entryId}` : entryId
+    preview && !entryId.startsWith('drafts.') ? `drafts.${entryId}` : entryId;
 
   const documents = await sanityClient.fetch(
     `*[_id == "${entryIdQuery}"]{"content":content.${locale}}`
-  )
+  );
 
   if (documents.length === 0) {
     // Handle the case when no matching document is found
     // entryId = _id of the Shopstory Block inside Sanity Studio
-    throw new Error('No matching document found')
+    throw new Error('No matching document found');
   }
 
-  return documents[0].content
+  return documents[0].content;
 }
 
-export default ShopstoryBlockPage
+export default ShopstoryBlockPage;
